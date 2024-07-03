cfde5b19ca4a136f4bfa065c0536675457209b94
fredrikcelin <fredrik.celin@gmail.com>
2024-07-03 05:14:30 +0000

    Ts

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import streamlit as st
 
 """
-# Welcome to Streamlit 007!
+# Welcome to Streamlit 008!
 
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
 If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
